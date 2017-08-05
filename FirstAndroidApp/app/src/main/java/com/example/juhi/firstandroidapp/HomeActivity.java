32e693d4f0067ad408a1f0617236e0d91c409ec8
Juhi <juhpark@csumb.edu>
2017-08-05 13:07:13 -0700

    Added butter knife functionality to button and text view

--- FirstAndroidApp/app/src/main/java/com/example/juhi/firstandroidapp/HomeActivity.java
+++ FirstAndroidApp/app/src/main/java/com/example/juhi/firstandroidapp/HomeActivity.java
@@ -5,16 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button clickMeButton;
 
+    @BindView(R.id.butter_button) Button butterButton;
+    @BindView(R.id.butter_knife_text_view_id) TextView butterTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
 
         clickMeButton = (Button) findViewById(R.id.click_me_button);
 
@@ -30,5 +40,17 @@ public class HomeActivity extends AppCompatActivity {
         Toast.makeText(this, "Did it again!!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, AboutActivity.class);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.butter_button)
+    public void onButterButtonClicked() {
+
+        Toast.makeText(this, "Did it again", Toast.LENGTH_SHORT).show();
+
+        if (butterTextView.isShown()) {
+            butterTextView.setVisibility(View.GONE);
+        } else {
+            butterTextView.setVisibility(View.VISIBLE);
+        }
     }
 }
