2fc7ecafac5a9b18d855d2ba07d0130e598c3059
Juhi <juhpark@csumb.edu>
2017-08-05 11:44:57 -0700

    Adding a click me button and shows a toast

--- FirstAndroidApp/app/src/main/java/com/example/juhi/firstandroidapp/HomeActivity.java
+++ FirstAndroidApp/app/src/main/java/com/example/juhi/firstandroidapp/HomeActivity.java
@@ -2,12 +2,26 @@ package com.example.juhi.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button clickMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        clickMeButton = (Button) findViewById(R.id.click_me_button);
+
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "did it!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
